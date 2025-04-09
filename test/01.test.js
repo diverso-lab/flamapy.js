0ec1c3dc21c86bb359f6b3b60da3803c7bd908d1
ilimaylla <maria.limaylla@udc.es>
2025-04-09 13:12:21 +0200

    Updates for multiple parameters

--- test/01.test.js
+++ test/01.test.js
@@ -1,4 +1,4 @@
-import Flamapy from "../lib/flamapy.js";
+import { Flamapy } from "../lib/flamapy.js";
 import { fileURLToPath } from "url";
 import path from "path";
 import assert from "assert";
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const filePath = path.resolve(__dirname, "./uvls/test.uvl");
+const fileConfig = path.resolve(__dirname, "./config.csvconf");
 const filePathTxt = path.resolve(__dirname, "./test.txt");
 const filePathIncorrect = path.resolve(__dirname, "./uvls/test5.uvl");
 const options = {
@@ -108,4 +109,94 @@ test("Flamapy operations - satisfiable", async () => {
 test("Flamapy - Incorrect path", async () => {
     const flamapy = new Flamapy(filePathIncorrect,options);
     assert.deepEqual(flamapy.fileContent.slice(-4),'.uvl');
+});
+
+test("Flamapy operations - Commonality", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    const result = await flamapy.commonality(fileConfigValue);
+    assert.deepEqual(result,1.0);
+});
+
+test("Flamapy operations - Commonality - Incorrect parameter", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const result = await flamapy.commonality('');
+    assert.strictEqual(result, undefined);
+});
+
+test("Flamapy operations - filterFeatures", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    const result = await flamapy.filterFeatures(fileConfigValue);
+    assert.deepEqual(result,'');
+});
+
+test("Flamapy operations - filterFeatures - Incorrect parameter", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const result = await flamapy.filterFeatures('');
+    assert.strictEqual(result, undefined);
+});
+
+test("Flamapy operations - featureAncestors", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const result = await flamapy.featureAncestors("Security");
+    assert.deepEqual(normalizeString(result), normalizeString("P(1): UserManagement<br>P(2): Onlineshop"));
+});
+
+test("Flamapy operations - featureAncestors - Incorrect parameter", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const result = await flamapy.featureAncestors("Sea");
+    assert.strictEqual(result, undefined);
+});
+
+test("Flamapy operations - satisfiable Configuration", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    const result = await flamapy.satisfiableConfiguration(fileConfigValue);
+    assert.deepEqual(result,false);
+});
+
+test("Flamapy operations - satisfiable Configuration - full configuration False", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    const result = await flamapy.satisfiableConfiguration(fileConfigValue, false);
+    assert.deepEqual(result,false);
+});
+
+test("Flamapy operations - satisfiable Configuration - full configuration True", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    const result = await flamapy.satisfiableConfiguration(fileConfigValue, true);
+    assert.deepEqual(result,false);
+});
+
+test("Flamapy operations - satisfiable Configuration - Incorrect parameter", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    try {
+        const result = await flamapy.satisfiableConfiguration(fileConfigValue, 0);
+    } catch (error) {
+        assert.deepEqual(error.message.substring(0, 5),'ERROR'); 
+    }
+});
+
+test("Flamapy operations - satisfiable Configuration - Incorrect parameter 2", async () => {
+    const flamapy = new Flamapy(filePath,options);
+    await flamapy.initialize();
+    const fileConfigValue = fs.readFileSync(fileConfig, "utf8").replace(/\r\n/g, "\n")
+    try {
+        const result = await flamapy.satisfiableConfiguration(fileConfigValue, '');
+    } catch (error) {
+        assert.deepEqual(error.message.substring(0, 5),'ERROR'); 
+    }
 });
